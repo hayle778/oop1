a8c71d3b61b7b9cf266ed711f81b4f60a77f6192
GBTC407012UR <GBTC407012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-11-18 16:46:25 -0500

    oop1

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import com.company.Rectangle;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double width =0.0 ;
+        double height = 0.0;
+        double Area = 0.0;
+        double perimeter = 0.0;
+
+     ArrayList<Rectangle> rectangleArray= new ArrayList <>();
+        Rectangle first = new Rectangle(4.0, 40);
+        Rectangle second = new Rectangle(3.5,35.9);
+
+
+        first.getArea();
+        first.getPerimeter();
+        second.getArea();
+        second.getPerimeter();
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+    }
+}
